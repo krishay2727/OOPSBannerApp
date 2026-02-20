407a51f355fe32efe9b817b85aca8f587ebc3102
krishay2727 <youremail@example.com>
2026-02-21 02:00:07 +0530

    UC3: Refactored banner using String.join() for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -7,17 +7,44 @@
  * @version 2.0
  */
 
+
 public class OOPSBannerApp {
 
-  
     public static void main(String[] args) {
-        // Each letter (O, O, P, S) is 9 characters wide, aligned properly
-        System.out.println("  *****   " + "  *****   " + " ******** " + " ******** ");
-        System.out.println(" *     *  " + " *     *  " + " *       *" + " *        ");
-        System.out.println(" *     *  " + " *     *  " + " *       *" + " *        ");
-        System.out.println(" *     *  " + " *     *  " + " ******** " + " ******** ");
-        System.out.println(" *     *  " + " *     *  " + " *         " + "       *  ");
-        System.out.println(" *     *  " + " *     *  " + " *         " + "       *  ");
-        System.out.println("  *****   " + "  *****   " + " *         " + " ******** ");
+
+        String[] O = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+
+        String[] P = {
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+
+        String[] S = {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+
+        for (int i = 0; i < 7; i++) {
+            String line = String.join("   ", O[i], O[i], P[i], S[i]);
+            System.out.println(line);
+        }
     }
 }
