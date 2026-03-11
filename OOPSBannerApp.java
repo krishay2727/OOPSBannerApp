600f9f06a899e0bd74308aea7b31c73ac5da1f7d
krishay2727 <youremail@example.com>
2026-03-11 21:05:37 +0530

    UC6 implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,49 +1,53 @@
-/**
- * OOPSBannerApp
- *
- * Prints OOPS in Banner format.
- *
- * @author Arnav
- * @version 2.0
- */
-
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] O = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
+        String[] banner = {
+
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    " ***** ",
+                    " ***** "),
+
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *"),
+
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *"),
+
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    " ***** ",
+                    " ***** "),
+
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *"),
+
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *"),
+
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    " ***** ",
+                    " ***** ")
         };
 
-        String[] P = {
-                "****** ",
-                "*     *",
-                "*     *",
-                "****** ",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-
-        String[] S = {
-                " ***** ",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ***** "
-        };
-
-        for (int i = 0; i < 7; i++) {
-            String line = String.join("   ", O[i], O[i], P[i], S[i]);
+        for (String line : banner) {
             System.out.println(line);
         }
     }
